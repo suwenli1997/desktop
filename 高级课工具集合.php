b34e1614a54b77964a0642bdec82760e7f9dd265
suwenli1997 <37822796+suwenli1997@users.noreply.github.com>
2018-03-27 11:53:24 +0800

    Add files via upload

---
+++ 高级课工具集合.php
@@ -0,0 +1,29 @@
+1.html在线格式化--》整理代码的规范性
+    网站:http://tool.oschina.net/codeformat/html
+
+2.百度编辑器Ueditor
+    网址:http://ueditor.baidu.com/website/   使用Ueditor的时候注意图片的路径设置在ueditor/php/config.json文件里面
+
+3.md5在线解密破解
+    网址:http://www.cmd5.com/
+
+4.redis扩展包
+    http://pecl.php.net/package/redis
+
+5.支付宝接口调用
+    https://www.alipay.com/
+
+6.短信接口的调用(云之讯)
+    邮箱:2681483208@qq.com
+    密码:swlaitym1314
+    手机:18275864183
+    http://www.ucpaas.com/?utm_source=baidu&utm_medium=ppc&utm_term=%E5%93%81%E8%B7%91%E6%A0%87%E9%A2%98&utm_content=&utm_campaign=pinpao&id=103014
+
+7.github.com
+    账号:suwenli1997
+    密码:swlaitym1314
+    邮箱:2681483208@qq.com
+    个人网址:https://github.com/suwenli1997/PHP-.git
+
+8.聚合数据(https://www.juhe.cn/)
+    8.1  聚合数据是一个为智能手机开发者，网站站长，移动设备开发人员及图商提供原始数据API服务的综合性云数据平台。包含手机聚合，网站聚合，LBS聚合三部分，其功能类似于Google APIS。
